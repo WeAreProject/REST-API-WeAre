8b878e087233f333679320beb7ec746f3b546d8d
JohanDJ0 <johansinoe2049@gmail.com>
2025-03-28 20:27:25 -0600

    build

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://wearefront-production.up.railway.app"],
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   })
